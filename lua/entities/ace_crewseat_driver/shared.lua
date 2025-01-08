fac50f6c55807c42adfe035b33ef5779ca1466ba
Shark731 <104893060+Shark73176@users.noreply.github.com>
2025-01-08 18:37:23 +0300

    Delete lua directory

--- lua/entities/ace_crewseat_driver/shared.lua
+++
@@ -1,8 +0,0 @@
-DEFINE_BASECLASS( "base_wire_entity" )
-
-ENT.Type = "anim"
-ENT.PrintName = "Crewseat-Driver"
-ENT.Author = "RDC"
-ENT.Category = "ACE-Crewseats"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
--- lua/entities/ace_crewseat_loader/shared.lua
+++
@@ -1,8 +0,0 @@
-DEFINE_BASECLASS( "base_wire_entity" )
-
-ENT.Type = "anim"
-ENT.PrintName = "Crewseat-Loader"
-ENT.Author = "RDC"
-ENT.Category = "ACE-Crewseats"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
